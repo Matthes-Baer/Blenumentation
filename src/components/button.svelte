e147a3f62edaffcac2ebb14531de0f1a190f48cb
Matthes-Baer <Matthes.baer@yahoo.de>
2022-11-06 20:34:42 +0100

    feat: updated button.svelte with tick & reactive variable logic from course

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
-	export let testString: string;
+	// Testing around following youtube course
+	import { tick } from 'svelte';
+	let count: number = 2;
+	$: reactivelVariable = count * 3;
+	const tickFunction = async () => {
+		count++;
+		// is asynchronous / helps with showing the DOM update
+		await tick();
+		console.log(`Tick: ${reactivelVariable}`);
 
+		// alternative to async/await logic
+		// tick().then(() => {
+		// 	console.log(`after Tick: ${reactivelVariable}`);
+		// });
+	};
+
+	export let testString: string;
 	export const mockFunction = (): void => {
 		testString = 'new one';
 	};
@@ -10,4 +25,5 @@
 	<h1>This is a button:</h1>
 	<h2 data-testid="testVariableTestID">{testString}</h2>
 	<button type="submit" on:click={mockFunction}>A button</button>
+	<button on:click={tickFunction}>reactive Variable and Tick Testing</button>
 </div>
