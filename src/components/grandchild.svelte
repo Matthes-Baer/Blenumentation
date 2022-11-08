263fd6079fa01d1008a304a4009d58bb36b90c7f
Matthes-Baer <Matthes.baer@yahoo.de>
2022-11-08 21:58:24 +0100

    feat: Added class with restriction in grandchild.svelte

--- src/components/grandchild.svelte
+++ src/components/grandchild.svelte
@@ -7,13 +7,25 @@
 		// dispatch logic to access function in the parent (event forwarding is used) route
 		dispatch('pageFunction', { id: 'Hello' });
 	};
+
+	let checker = 0;
 </script>
 
 <div style="margin-top: 100px;">
-	<h1>This is the grandchild.svelte component</h1>
+	<h1 class:positive={checker > 0}>
+		This is the grandchild.svelte component <button on:click={() => checker++}
+			>Make it greenyellow</button
+		>
+	</h1>
 	<h2 data-testid="testVariableTestID">{testString}</h2>
 	<button type="submit" on:click={mockFunction}>A button</button>
 	For event forwarding, you don't have to recreate any bubbling effects - just put "on:dispatchname"
 	on the component in between to hand it out to the further corresponding parent component. This also
 	works as attribute for elements (dispatch with the same name is output)
 </div>
+
+<style>
+	.positive {
+		background-color: greenyellow;
+	}
+</style>
