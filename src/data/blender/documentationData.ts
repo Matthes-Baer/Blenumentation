c813c77baa6b2d292a243be34f266f8266cb56db
Matthes-Baer <Matthes.baer@yahoo.de>
2022-11-11 21:19:46 +0100

    feat: Added new shortcuts from the udemy course

--- src/data/blender/documentationData.ts
+++ src/data/blender/documentationData.ts
@@ -7,7 +7,18 @@ const documentationData: documentationDataInterface[] = [
 	{ name: 'Global Location Reset', description: 'Click on the Mesh object', keystroke: 'Alt + G' },
 	{ name: 'Rotate Reset', description: 'Click on the Mesh object', keystroke: 'Alt + R' },
 	{ name: 'Scale Reset', description: 'Click on the Mesh object', keystroke: 'Alt + S' },
-	{ name: 'Add new Objects', description: 'Click somewhere', keystroke: 'Alt + A' }
+	{ name: 'Add new Objects', description: 'Click somewhere', keystroke: 'Shift + A' },
+	{
+		name: 'Multi Select',
+		description: 'Click on multiple objects to select them simultaneously',
+		keystroke: 'Shift'
+	},
+	{
+		name: 'Create a group with objects',
+		description:
+			'Click on multiple objects to group them together. It is important which object gets selected last since this decides where the main axis is for rotating the group, for example',
+		keystroke: 'Strg + J'
+	}
 ];
 
 export const sortedDocumentationData = documentationData.sort(
