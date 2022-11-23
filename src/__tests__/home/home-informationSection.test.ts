d180526df450fd6aea0e5f8e03a8a904f56389e9
Matthes-Baer <Matthes.baer@yahoo.de>
2022-11-23 22:37:27 +0100

    feat: Added tests for home and blender

--- src/__tests__/home/home-informationSection.test.ts
+++ src/__tests__/home/home-informationSection.test.ts
@@ -1,1 +1,12 @@
+import { render, screen } from '@testing-library/svelte';
 import InformationSection__SvelteComponent_ from '../../components/home/informationSection.svelte';
+
+describe('information section component test', () => {
+	test('render information section component', () => {
+		render(InformationSection__SvelteComponent_);
+		const informationText = screen.getByText(
+			/This project is my first Svelte application and is mainly used for learning purposes/
+		);
+		expect(informationText).toBeInTheDocument();
+	});
+});
--- src/__tests__/home/home-page.test.ts
+++ src/__tests__/home/home-page.test.ts
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/svelte';
 import HomePage from '../../routes/+page.svelte';
+import Layout from '../../routes/+layout.svelte';
 
 describe('Test home page', () => {
 	test('render home page', () => {
 		render(HomePage);
 		const heading = screen.getByRole('heading', { name: 'Home - start information' });
 		expect(heading).toBeInTheDocument();
+	});
+});
+
+describe('Test main layout component', () => {
+	test('render layout bar', () => {
+		render(Layout);
+		const layoutBarEntry = screen.getByText('Blender Hotkey Documentation');
+		expect(layoutBarEntry).toBeInTheDocument();
 	});
 });
