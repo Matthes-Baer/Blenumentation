a86efaa7645328ea6f09e0268521f8f4b08b46d2
Matthes-Baer <Matthes.baer@yahoo.de>
2023-01-21 18:10:31 +0100

    feat: Updated to-do information

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -7,15 +7,15 @@
 		<li>Home Page General Styling</li>
 		<li>Main Navbar Styling</li>
 		<li>Blender Hotkey doc styling</li>
-		<li>Weitere Inhalte überlegen - Import für Three.js, ...</li>
+		<li>
+			Weitere Inhalte überlegen - Import für Three.js (inkl. Baking), Shading ... - als Routen?
+		</li>
 		<li>Einbindung für store überlegen</li>
 		<li>Einbindung für reactive statemets & für reactive Zugriff auf Store überlegen</li>
-		<li>Info Route bearbeiten mit Informationen</li>
-		<li>Info Route bearbeiten mit Styling</li>
+		<li>SecondPartInformation ggf. anpassen mit anderem Text</li>
 		<li>Unit Tests hinzufügen</li>
 		<li>Child Components mit Props etc.</li>
 		<li>responsive designs</li>
-		<li>Alles auf eine Route mit Components?</li>
 		<li>
 			https://svelte.dev/tutorial/basics durchgehen für weitere Dinge zum Einfügen oder für Hilfe
 		</li>
